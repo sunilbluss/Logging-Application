a0f127e9e81f574aac97b29a5924a1282072e786
sunil bluss <sunilbluss@gmail.com>
2016-03-13 09:33:00 +0530

    initial Commit For Peoject Setup

---
+++ src/main/java/com/bluss/taskmanagerlogging/App.java
@@ -0,0 +1,13 @@
+package com.bluss.taskmanagerlogging;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
